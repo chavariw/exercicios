c3f25e7daae1b29d8e93c45450fc0fde6e675136
wchavari <wander.chavari@kangu.com.br>
2022-10-24 19:56:20 -0300

    Alterada classe Temperatura

--- src/fundamentos/Temperatura.java
+++ src/fundamentos/Temperatura.java
@@ -4,7 +4,7 @@ public class Temperatura {
 
     public static void main(String[] args) {
         //(ºF - 32) x 5/9 = ºC
-        double tempF = 21.0;
+        double tempF = 86.0;
         final int AJUSTE = 32;
         final double FATOR = 5/9.0;
         double tempC = (tempF - AJUSTE) * FATOR;
